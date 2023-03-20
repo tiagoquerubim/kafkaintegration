3ca978e67130ba0c2ac0fe832510a6d186c54ef4
tiagoquerubim <tquerubim@gmail.com>
2023-03-20 15:37:23 -0300

    Adding a producer and a consumer.

---
+++ src/main/java/com/kafkaintegration/kafkaintegration/configuration/KafkaConsumerConfig.java
@@ -0,0 +1,2 @@
+package com.kafkaintegration.kafkaintegration.configuration;public class KafkaConsumerConfig {
+}
---
+++ src/main/java/com/kafkaintegration/kafkaintegration/util/ResponseSupport.java
@@ -0,0 +1,2 @@
+package com.kafkaintegration.kafkaintegration.util;public class ResponseSupport {
+}
