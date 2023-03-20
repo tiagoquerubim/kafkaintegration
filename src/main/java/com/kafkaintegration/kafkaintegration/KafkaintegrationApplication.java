8197ce826566c5c58381c0eccf7ced132a2f4997
tiagoquerubim <tquerubim@gmail.com>
2023-03-20 12:23:42 -0300

    Init

---
+++ src/main/java/com/kafkaintegration/kafkaintegration/KafkaintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaintegration.kafkaintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaintegration/kafkaintegration/KafkaintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaintegration.kafkaintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
